2903e4c85f3620a9845d417aa1383edaf6a352d9
Anil Maharjan <anil@anilmaharjan.com.np>
2020-12-27 20:44:48 +0100

    filter dropdown

---
+++ components/02-molecules/06-filter-dropdown/filter-dropdown.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  context: {
+    label: "Size",
+    options: [
+      { label: "S", value: "S" },
+      { label: "M", value: "M" },
+      { label: "L", value: "L" },
+    ],
+  },
+};
