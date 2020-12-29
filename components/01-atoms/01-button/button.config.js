d8db67434ec95f40ee3128628e378aabea347175
Pratima Maharjan <prati.ma@hotmail.com>
2020-12-29 11:34:42 +0100

    Radio/checkbox buttons

--- components/01-atoms/01-button/button.config.js
+++ components/01-atoms/01-button/button.config.js
@@ -3,4 +3,26 @@ module.exports = {
     icon: "search",
     text: "Search",
   },
+  variants: [
+    {
+      name: "radio-buttons",
+      context: {
+        fieldname: "Size",
+        options: [
+          {
+            label: "S",
+            value: "s",
+          },
+          {
+            label: "M",
+            value: "m",
+          },
+          {
+            label: "L",
+            value: "l",
+          },
+        ],
+      },
+    },
+  ],
 };
