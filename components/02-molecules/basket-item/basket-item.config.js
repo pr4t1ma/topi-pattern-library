fb10c672a7cd7f38c9fbd99c3ecc79abbe84383e
Pratima Maharjan <prati.ma@hotmail.com>
2021-01-02 12:14:46 +0100

    Basket Items

--- components/02-molecules/basket-item/basket-item.config.js
+++ components/02-molecules/basket-item/basket-item.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   context: {
     name: "Kalo Topi",
-    delete: { text: "delete", label: "", icon: "trash" },
-    button: { text: "move to wishlist" },
+    qty: 3,
+    price: {
+      new: "500.-",
+    },
+    image: {
+      imgAttr: "160/160?0.0755426283",
+      alt: "product Kalo Topi",
+    },
+    deleteBtn: { text: "Delete", icon: "trash" },
+    wishlistBtn: { text: "Move to wishlist" },
   },
 };
