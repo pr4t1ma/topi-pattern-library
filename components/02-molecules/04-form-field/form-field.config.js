52797417c05ec7f70a4df800a8bcf6c8b69f9a93
Pratima Maharjan <prati.ma@hotmail.com>
2021-01-10 16:15:02 +0100

    progress bar and login-form

---
+++ components/02-molecules/04-form-field/form-field.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  context: {
+    label: "First name",
+    placeholder: "First Name",
+    hint: "Password must be 6 characters or more.",
+  },
+};
---
+++ components/02-molecules/55-login-form/login-form.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  context: {
+    login: {
+      text: "Login",
+    },
+    register: {
+      text: "Register",
+    },
+    firstname: {
+      label: "First name",
+      type: "text",
+      placeholder: "Enter your first name",
+    },
+    lastname: {
+      label: "Last name",
+      type: "text",
+      placeholder: "Enter your last name",
+    },
+    email: {
+      label: "email",
+      type: "email",
+      placeholder: "Email",
+    },
+    password: {
+      label: "Password",
+      type: "Password",
+      placeholder: "Password",
+      hint: "Password must be more than 6 characters long.",
+    },
+  },
+};
