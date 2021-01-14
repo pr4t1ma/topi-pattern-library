b78318d3045243ed2df8dd2e313d27e5e95aff8f
Pratima Maharjan <prati.ma@hotmail.com>
2021-01-14 16:29:53 +0100

    review  bar is complete

---
+++ components/01-atoms/08-rating-countbar/rating-countbar.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  context: {
+    rating: 5,
+    barWidth: 60,
+  },
+};
---
+++ components/02-molecules/08-review-rating/review-rating.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  context: {
+    rating4: {
+      rating: 4,
+      barWidth: 40,
+    },
+    rating3: {
+      rating: 3,
+      barWidth: 30,
+    },
+    rating2: {
+      rating: 2,
+      barWidth: 20,
+    },
+    rating1: {
+      rating: 1,
+      barWidth: 10,
+    },
+  },
+};
