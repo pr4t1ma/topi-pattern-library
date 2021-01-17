9e10a69604a572c8129b5c2f6dcca53cb459656e
Pratima Maharjan <prati.ma@hotmail.com>
2021-01-17 16:09:30 +0100

    user-review-list ist completed

---
+++ components/02-molecules/72-user-review-list/user-review-list.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  context: {
+    items: [
+      {
+        title: "lena",
+        date: "10,jan,2018",
+        image: {
+          imgAttr: "160/160?0.0755426283",
+        },
+      },
+      {
+        title: "Zana",
+        date: "12, june,2019",
+        image: {
+          imgAttr: "160/160?2.0755426283",
+        },
+      },
+      {
+        title: "Lusi",
+        date: "22,Nov,2020",
+        image: {
+          imgAttr: "160/160?1.0755426283",
+        },
+      },
+    ],
+  },
+};
